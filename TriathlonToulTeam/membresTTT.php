b8654a8680cb4aa13728a24d7046fd3e8686945d
cyrille marc <cyrillemarc1@gmail.com>
2022-08-19 14:28:06 +0200

    fix request member.php

--- TriathlonToulTeam/membresTTT.php
+++ TriathlonToulTeam/membresTTT.php
@@ -1,9 +1,9 @@
 <?php
 try {
     $bdd = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_ecb86cfcf145222;charset=utf8', "beceab70a9685f", "134b075f");
-    $req = $bdd->prepare('SELECT Nom, Email FROM membres');
+    $req = $bdd->prepare('SELECT * FROM membres');
     $req->execute();
-    $Athlete = $req->fetchAll();
+    $Athletes = $req->fetchAll();
     $req->closeCursor();
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
@@ -18,32 +18,25 @@ try {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&family=Oswald:wght@200&display=swap" rel="stylesheet">
-    <title>Anniversaire Zoé</title>
+    <title>Espace membres</title>
 </head>
 
 <header>
     <div class="container">
-        <h1>Anniversaire de Zoé : 20 ans déjà !</h1>
+        <h1>ESPACE MEMBRES TTT</h1>
     </div>
 </header>
 
 <body>
-    <div id="content"></div>
-    <div class="photos">
-        <div id="photo"><a href="./diaporama.php"><img src="./diaporama.png"></a></div>
-        <div id="photo"><a href="./cadeau.php"><img src="./cadeau.png"></a></div>
-        <div id="photo"><a href="./PostMessage.php"><img src="./message.png"></a></div>
-    </div>
-    </div>
-    <h3 id="titleMessage">Messages de la famille:</h3>
-    <div>
-        <table>
-            <?php foreach ($messages as $message) : ?>
-                <td><?php echo '"' . $Athlete['Nom'] . '"'; ?></td>
-                <td><?php echo "-" . $Athlete['Email'] . "-"; ?></td>
-
-            <?php endforeach; ?>
-        </table>
+    <div id="content">
+        <div>
+            <table>
+                <?php foreach ($Athletes as $Athlete) : ?>
+                    <td><?php echo '"' . $Athlete['Nom'] . '"'; ?></td>
+                    <td><?php echo "-" . $Athlete['Email'] . "-"; ?></td>
+                <?php endforeach; ?>
+            </table>
+        </div>
     </div>
 </body>
 
