3c4f1ef563040dfa7ea85b5d6a94098d7a3a6f34
cyrille marc <cyrillemarc1@gmail.com>
2022-08-19 11:27:02 +0200

    fix

--- TriathlonToulTeam/Login.php
+++ TriathlonToulTeam/Login.php
@@ -32,6 +32,7 @@
                         <input type="text" name="Nom" id="Nom"><br>
                         <h3>Mot de passe</h3>
                         <input type="text" name="Mdp" id="Mdp"><br>
+                        <script type='text/javascript'>print('$erreur');</script>
                         <button type="submit" value="">Valider</button>
             </form>
         </div>
@@ -62,8 +63,7 @@
         }
         else{
             $erreur =  "Mauvais identifiants";
-            echo "<script type='text/javascript'>alert('$erreur');</script>";
-            
+
         }
     }
 
